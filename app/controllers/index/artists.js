cf4912a2f0da1ce6d2661f1205dff9a887831dab
Melissa Porto <melroman26@gmail.com>
2015-08-26 23:31:44 -0700

    Starting to set up related files

--- app/controllers/index/artists.js
+++ app/controllers/index/artists.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['artistName'],
-  artistName: ''
+  artistName: '',
+  select(id) {
+    this.transitionToRoute('index.related', {
+      queryParams: {id: this.get('id')}
+    });
+      console.log(id);
+      console.log(queryParams);
+  }
 });
--- app/routes/index/related.js
+++ app/routes/index/related.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(artistId){
-    // return $.ajax({
-    //     url: 'https://api.spotify.com/v1/artists/' + artistId + '/related-artists'
-    //   });
+  queryParams: {
+    id: {
+      refreshModel: true
+    }
+  },
+  model(queryParams){
+    var related =  $.ajax({
+        url: 'https://api.spotify.com/v1/artists/' + id + '/related-artists',
+        data: {
+          query: queryParams.id,
+          type: 'artist'
+        }
+      }).then(function(res){
+        return {artists: res.artists.items}
+      });
+    return a;
     }
 });
