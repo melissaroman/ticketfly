83c5a1eb3851f5f47780e264c1443d07c6db5f0f
Melissa Porto <melroman26@gmail.com>
2015-08-30 14:56:38 -0700

    Adds link to helper to artists page

--- app/controllers/index/artists.js
+++ app/controllers/index/artists.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   queryParams: ['artistName'],
-  artistName: '',
-  actions: {
-    select(id) {
-      this.transitionToRoute('index.related', {
-        queryParams: {id: id}
-      });
-    }
-  }
+  artistName: ''
 });
